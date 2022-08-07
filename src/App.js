c48c75c6f27a8cca66face5a2731b92a597a6b70
Ermias <yetabalij@gmail.com>
2022-08-07 13:07:41 +0300

    login component imported and routing implemented.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
