255b00149a64bb0b859c03b96d95e12f2fddace4
Ermias <yetabalij@gmail.com>
2022-08-07 14:43:39 +0300

    Login button completed.

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,6 +20,12 @@ const Login = (props) => {
           <h1>Welcom to your professional community</h1>
           <img src="/images/login-hero.svg" alt="hero" />
         </Hero>
+        <Form>
+          <Google>
+            <img src="/images/google.svg" alt="google" />
+            Sign in with Google
+          </Google>
+        </Form>
       </Section>
     </Container>
   );
@@ -123,6 +129,33 @@ const Hero = styled.div`
       position: initial;
       height: initial;
     }
+  }
+`;
+const Form = styled.div`
+  margin-top: 100px;
+  width: 408px;
+  @media (max-width: 768px) {
+    margin-top: 20px;
+  }
+`;
+const Google = styled.button`
+  display: flex;
+  justify-content: center;
+  background-color: #fff;
+  align-items: center;
+  height: 56px;
+  width: 100%;
+  border-radius: 28px;
+  box-shadow: inset 0 0 0 1px rgba(0 0 0 / 20%),
+    inset 0 0 0 1px rgba(0 0 0 / 0%), inset 0 0 0 1px rgba(0 0 0 / 0);
+  vertical-align: middle;
+  z-index: 0;
+  transition-duration: 167ms;
+  font-size: 20px;
+  color: rgba(0, 0, 0, 0.6);
+  &:hover {
+    background-color: rgba(207, 207, 207, 0.25);
+    color: rgba(0, 0, 0, 0.75);
   }
 `;
 
