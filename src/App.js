9ba373346fa27134fc7dba713ea727b5784e82ea
Ermias <yetabalij@gmail.com>
2022-08-07 12:55:03 +0300

    reseting default styling of the browser.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  );
 }
 
 export default App;
